b9932f527f8bbcc7e1c4d908b3d269580b0e0ee4
Tolu Olorode <146240429+ToluOlorode@users.noreply.github.com>
2024-06-11 00:28:32 +0100

    Create quiz_game.py

---
+++ quiz_game.py
@@ -0,0 +1,46 @@
+print("---------------------")
+print("Welcome to my Sims 4 quiz!")
+print("---------------------")
+
+playing = input("\nDo you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("\nOkay! Let's play :)")
+score = 0
+
+answer = input("\nWhat is the primary in-game currency used by Sims to purchase items and build houses in 'The Sims 4'? ")
+
+if answer.lower() == "simoleons":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+    
+answer = input("\nWhat expansion introduced the ability for Sims to live in apartments and experience city life? ")
+
+if answer.lower() == "city living":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+    
+answer = input("\nWhich of the following careers is NOT a base game career option in 'The Sims 4'? ")
+
+if answer.lower() == "detective":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("\nWhat feature allows Sims to have unique personalities and affect their behavior and emotions in 'The Sims 4'? ")
+
+if answer.lower() == "traits":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+    
+print(f"You got {score} questions correct!") 
+print(f"You got {score / 4 * 100}%")
